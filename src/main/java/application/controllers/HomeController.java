30e9e90e3578eb6cda1ac075bb944d3617a5d7e7
lucymara <luci.carla@live.com>
2021-08-31 19:31:04 +0000

    atializaçao

--- src/main/java/application/controllers/HomeController.java
+++ src/main/java/application/controllers/HomeController.java
@@ -1,14 +1,24 @@
 package application.controllers;
 
+
+
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
+
 @Controller
+
 public class HomeController {
+
     @RequestMapping(method=RequestMethod.GET)
+
     public String index() {
-      return "home/index.jsp";
+
+        return "home/index.jsp";
 
     }
 
